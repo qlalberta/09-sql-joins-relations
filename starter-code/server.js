498d3618bbdda54aa6186b1e7789c118407c76e5
ql24 <qlalberta@gmail.com>
2017-07-20 21:49:01 -0700

    finished the updateRecord function

--- starter-code/server.js
+++ starter-code/server.js
@@ -67,11 +67,13 @@ app.post('/articles', function(request, response) {
       body
   )
   VALUES ($1, $2, $3, $4, $5);`, // DONE: Write a SQL query to insert the new article using the author_id from our previous query
-      [author_id,
+      [
+        author_id,
         request.body.title,
         request.body.category,
         request.body.publishedOn,
-        request.body.body], // DONE: Add the data from our new article, including the author_id, as data for the SQL query.
+        request.body.body
+      ], // DONE: Add the data from our new article, including the author_id, as data for the SQL query.
       function(err) {
         if (err) console.error(err);
         response.send('insert complete');
@@ -85,16 +87,33 @@ app.put('/articles/:id', function(request, response) {
   // an author_id property, so we can reference it from the request.body.
   // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `UPDATE authors
+     SET author = $1, "authorUrl" = $2
+     WHERE author_id = $3;
+    `,
+    [
+      request.body.author,
+      request.body.authorUrl,
+      request.body.author_id
+    ]
   )
   .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      ``,
-      []
+      `UPDATE articles
+       SET author_id = $1, title = $2, category = $3, "publishedOn" = $4, body = $5
+       WHERE article_id = $6;
+      `,
+      [
+        request.body.author_id,
+        request.body.title,
+        request.body.category,
+        request.body.publishedOn,
+        request.body.boby,
+        request.body.article_id
+      ]
     )
   })
   .then(function() {
